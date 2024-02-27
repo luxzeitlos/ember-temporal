8ec8b6bbb97801ad93ae1a39e765e542ef606b33
Lux <lux@lux.name>
2024-02-27 13:17:55 +0100

    lint fixes

--- tests/unit/transforms/temporal-zoned-date-time-test.js
+++ tests/unit/transforms/temporal-zoned-date-time-test.js
@@ -8,7 +8,7 @@ module('Unit | Transform | temporal zoned date time', function (hooks) {
   test('it can deserialize', function (assert) {
     let transform = this.owner.lookup('transform:temporal-zoned-date-time');
     const obj = transform.deserialize(
-      '2020-08-05T20:06:13+02:00[Europe/Berlin]'
+      '2020-08-05T20:06:13+02:00[Europe/Berlin]',
     );
     assert.ok(obj instanceof Temporal.ZonedDateTime);
     assert.strictEqual(obj.year, 2020);
@@ -26,7 +26,7 @@ module('Unit | Transform | temporal zoned date time', function (hooks) {
   test('it can serialize', function (assert) {
     let transform = this.owner.lookup('transform:temporal-zoned-date-time');
     const str = transform.serialize(
-      Temporal.ZonedDateTime.from('2020-08-05T20:06:13+05:45[+05:45]')
+      Temporal.ZonedDateTime.from('2020-08-05T20:06:13+05:45[+05:45]'),
     );
     assert.strictEqual(str, '2020-08-05T20:06:13+05:45[+05:45]');
   });
